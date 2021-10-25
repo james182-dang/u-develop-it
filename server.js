63b872c30a91d0a5946f02a0059f5e6c558d6f14
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-10-24 20:48:01 -0500

    set up mySQL connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'HorseBucketChalice298#6',
+        database: 'election'
+    },
+    console.log('Connected to the election database. ')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
