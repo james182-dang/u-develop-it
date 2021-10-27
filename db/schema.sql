7e01d5e469b38489c0eaa52f68dd0aa2a33b30b4
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-10-27 17:04:08 -0500

    push 2 for BCS

--- db/schema.sql
+++ db/schema.sql
@@ -9,10 +9,7 @@ CREATE TABLE voters (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT uc_voter UNIQUE (voter_id),
-    CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE votes (
